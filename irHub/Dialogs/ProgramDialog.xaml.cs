177934f3bf20b4b5aa83e988809f89d92107bfa8
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2024-11-21 16:15:49 +0100

    Added ability to change the icon for a program to a custom icon, reset it etc & started implementing more logic for adding new programs

--- irHub/Dialogs/ProgramDialog.xaml.cs
+++ irHub/Dialogs/ProgramDialog.xaml.cs
@@ -5,11 +5,15 @@ using HandyControl.Controls;
 using irHub.Classes;
 using irHub.Classes.Models;
 using irHub.Windows;
+using Microsoft.Win32;
+using Path = System.IO.Path;
 
 namespace irHub.Dialogs;
 
 public partial class ProgramDialog : INotifyPropertyChanged
 {
+    private readonly bool _isNew;
+    
     private Program? _program;
     public Program? Program
     {
@@ -32,23 +36,6 @@ public partial class ProgramDialog : INotifyPropertyChanged
         }
     }
     
-    internal ProgramDialog(ref Program program)
-    {
-        InitializeComponent();
-        if (Application.Current.MainWindow is MainWindow mainWindow)
-            Owner = mainWindow;
-
-        OriginalProgram = Global.DeepCloneT(program);
-        // Manually copy over properties that can't be parsed to JSON (1/2)
-        // todo make it possible to convert these properties to JSON (maybe base64 encoding it?) 
-        OriginalProgram.Icon = program.Icon;
-        OriginalProgram.Process = program.Process;
-        OriginalProgram.ActionButton = program.ActionButton;
-        
-        Program = program;
-        DataContext = Program;
-    }
-    
     #region INotifyPropertyChanged
     public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -58,6 +45,29 @@ public partial class ProgramDialog : INotifyPropertyChanged
     }
 
     #endregion
+    
+    internal ProgramDialog(ref Program program, bool isNew = false)
+    {
+        InitializeComponent();
+        if (Application.Current.MainWindow is MainWindow mainWindow)
+            Owner = mainWindow;
+        
+        Program = program;
+        DataContext = Program;
+        
+        if (!program.UseExecutableIcon) 
+            ResetIconButton.Visibility = Visibility.Visible;
+        
+        _isNew = isNew;
+        if (_isNew) return;
+
+        OriginalProgram = Global.DeepCloneT(program);
+        // Manually copy over properties that can't be parsed to JSON (1/2)
+        // todo make it possible to convert these properties to JSON (maybe base64 encoding it?) 
+        OriginalProgram.Icon = program.Icon;
+        OriginalProgram.Process = program.Process;
+        OriginalProgram.ActionButton = program.ActionButton;
+    }
 
     private void SaveButton_OnClick(object sender, RoutedEventArgs e)
     {
@@ -73,18 +83,66 @@ public partial class ProgramDialog : INotifyPropertyChanged
     private void CancelButton_OnClick(object sender, RoutedEventArgs e)
     {
         // Cancel changes
-        if (OriginalProgram is not null && Program is not null)
+
+        if (_isNew)
+        {
+            // todo handle new
+        }
+        
+        if (!_isNew && OriginalProgram is not null && Program is not null)
         {
             Global.CopyProperties(OriginalProgram, Program);
             // Manually copy over properties that can't be parsed to JSON (2/2)
             Program.Icon = OriginalProgram.Icon;
             Program.Process = OriginalProgram.Process;
             Program.ActionButton = OriginalProgram.ActionButton;
-            
         }
         
         if (Application.Current.MainWindow is MainWindow mainWindow)
             mainWindow.Focus();
         Close();
     }
+
+    private void ShowNewIconDialog()
+    {
+        Program ??= new Program();
+        
+        // todo test if .ico works with GetIconFromFile!
+        var dialog = new OpenFileDialog
+        {
+            Filter = "Images / Icons (*.exe, *.jpg, *.jpeg, *.png, *.ico)|*.exe;*.jpg;*.jpeg;*.png;*.ico",
+            InitialDirectory = Path.GetDirectoryName(Program.FilePath),
+            Multiselect = false,
+            Title = "Please select an image. A resolution of at least 100x100 and 1:1 aspect ratio is recommended."
+        };
+
+        if (dialog.ShowDialog() is not true || dialog.FileName is "")
+        {
+            ResetToExecutableIcon();
+            return;
+        }
+        if (dialog.FileName == Program.FilePath)
+        {
+            ResetToExecutableIcon();
+            return;
+        }
+        
+        Program.Icon = Global.GetIconFromFile(dialog.FileName);
+        Program.UseExecutableIcon = false;
+        Program.IconPath = dialog.FileName;
+        ResetIconButton.Visibility = Visibility.Visible;
+    }
+
+    private void ResetToExecutableIcon()
+    {
+        Program ??= new Program();
+        Program.Icon = Global.GetIconFromFile(Program.FilePath);
+        Program.UseExecutableIcon = true;
+        Program.IconPath = "";
+        ResetIconButton.Visibility = Visibility.Hidden;
+    }
+
+    private void SelectNewIcon_OnClick(object sender, RoutedEventArgs e) => ShowNewIconDialog();
+
+    private void ResetIconButton_OnClick(object sender, RoutedEventArgs e) => ResetToExecutableIcon();
 }
