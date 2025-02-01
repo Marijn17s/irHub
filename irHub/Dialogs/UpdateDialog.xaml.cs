f117d937410a6bae33a4b5c6744d327302cfe79a
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2025-02-01 13:35:49 +0100

    Removed testing code from updatedialog

--- irHub/Dialogs/UpdateDialog.xaml.cs
+++ irHub/Dialogs/UpdateDialog.xaml.cs
@@ -14,10 +14,8 @@ public partial class UpdateDialog
     public UpdateDialog(SemanticVersion? currentVersion, UpdateInfo newVersion)
     {
         InitializeComponent();
-
-        var t = "# irHub changelogs\n\n## Version 0.9.10\nThis version is a small improvement in terms of performance and has some QoL updates and has a few bugfixes.\n\n## Version 0.9.9\n\nThis version brings a basic auto update system which will later be expanded upon to be more detailed.\n\nIt is recommended to install this new version, so you always stay up to date with the latest features!\n\n## Version 0.9.0\n\nI am glad to announce we finally have a workable version ready for release!\n\nIn this version you'll be able to add your favorite iRacing tools and let them automatically start whenever you start playing iRacing!\n\nNote that there are likely still some bugs; you can report them [here](https://github.com/Marijn17s/irHub/issues/new)\n\nThere is still a lot to be done, and I'm excited to see what we can make in the future.\n\n> SHA256: 492C74A6333C535C1FFE904A12759C7D8A535DBA899523EE3EF4DA87DC03CC41\n\n## Version 0.9.10\nThis version is a small improvement in terms of performance and has some QoL updates and has a few bugfixes.\n\n## Version 0.9.9\n\nThis version brings a basic auto update system which will later be expanded upon to be more detailed.\n\nIt is recommended to install this new version, so you always stay up to date with the latest features!\n\n## Version 0.9.0\n\nI am glad to announce we finally have a workable version ready for release!\n\nIn this version you'll be able to add your favorite iRacing tools and let them automatically start whenever you start playing iRacing!\n\nNote that there are likely still some bugs; you can report them [here](https://github.com/Marijn17s/irHub/issues/new)\n\nThere is still a lot to be done, and I'm excited to see what we can make in the future.\n\n> SHA256: 492C74A6333C535C1FFE904A12759C7D8A535DBA899523EE3EF4DA87DC03CC41";
-        var releaseNotes = t;
-        //var releaseNotes = newVersion.TargetFullRelease.NotesHTML;
+        
+        var releaseNotes = newVersion.TargetFullRelease.NotesHTML;
         string html = ReleaseNotesHelper.GenerateReleaseNotes(releaseNotes);
         string tempFile = Path.Combine(Path.GetTempPath(), "irHubTempReleaseNotes.html");
         File.WriteAllText(tempFile, html);
