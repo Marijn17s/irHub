55d736ae795a75b223b301b0d27674e37b5b91d8
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2025-02-17 21:45:41 +0100

    Center of the screen is now calculated manually

--- irHub/Dialogs/UpdateDialog.xaml.cs
+++ irHub/Dialogs/UpdateDialog.xaml.cs
@@ -15,20 +15,25 @@ public partial class UpdateDialog
     public UpdateDialog(SemanticVersion? currentVersion, UpdateInfo newVersion)
     {
         InitializeComponent();
-        
-        if (Application.Current.MainWindow is MainWindow mainWindow)
-            Owner = mainWindow;
-        
+        MoveToMainScreen();
+
         var releaseNotes = newVersion.TargetFullRelease.NotesHTML;
         string html = ReleaseNotesHelper.GenerateReleaseNotes(releaseNotes);
         string tempFile = Path.Combine(Path.GetTempPath(), "irHubTempReleaseNotes.html");
         File.WriteAllText(tempFile, html);
 
-        Title = $"irHub version {newVersion?.TargetFullRelease.Version} is available";
-        ReleaseNotesText.Text = $"irHub {newVersion?.TargetFullRelease.Version} is now available. Would you like to update now?";
+        Title = $"irHub version {newVersion.TargetFullRelease.Version} is available";
+        ReleaseNotesText.Text = $"irHub {newVersion.TargetFullRelease.Version} is now available. Would you like to update now?";
         if (currentVersion is not null)
-            ReleaseNotesText.Text = $"irHub {newVersion?.TargetFullRelease.Version} is now available. Version {currentVersion} is currently installed. Would you like to update now?";
+            ReleaseNotesText.Text = $"irHub {newVersion.TargetFullRelease.Version} is now available. Version {currentVersion} is currently installed. Would you like to update now?";
         WebView.Source = new Uri(tempFile);
+    }
+
+    private void MoveToMainScreen()
+    {
+        var screen = SystemParameters.WorkArea;
+        Left = screen.Left + (screen.Width - Width) / 2;
+        Top = screen.Top + (screen.Height - Height) / 2;
     }
 
     private void WebView_OnNavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e)
